71f709e81f13300823dc6e47c2c71abd26f81ccc
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-06-05 12:34:23 +0300

    set up the cofiguration file in the root directory

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+from flask import config
+
+class Config:
+    NEWS_API_BASE_URL= ''
+    NEWS_API_KEY = os.environ.get('NEWS_API_API')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
