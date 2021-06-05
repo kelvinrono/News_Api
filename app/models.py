dcf1122bbc479a6d4fad37dc8f73b7f9cfdf714d
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-06-05 15:49:02 +0300

    create the the source and class models

---
+++ app/models.py
@@ -0,0 +1,13 @@
+class Source:
+    def __init__(self,id, name):
+        self.name = name
+        self.id = id
+
+class Articles:
+    def __init__(self, urlToImage, description, publishedAt):
+        self.urlToImage = urlToImage
+        self.description = description
+        self.publishedAt = publishedAt
+
+        
+    
