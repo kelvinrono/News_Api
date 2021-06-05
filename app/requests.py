ed7fd8bf30fb3b36553c1ba67f4b8dc5a2bef42c
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-06-05 15:56:52 +0300

    import the models and set up the link the confuguration objects

---
+++ app/requests.py
@@ -0,0 +1,6 @@
+import urllib.request, json
+from .models import Source, Articles
+
+api_key = None
+base_url = None
+
--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 from flask import config
 
 class Config:
-    NEWS_API_BASE_URL= ''
+    NEWS_API_BASE_URL= 'https://newsapi.org/v2/everything?q={}&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_API')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
